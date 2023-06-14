b9b5cc839ca53a805846d4f4f62dffcababda509
avcrostian <avilescristian97@gmail.com>
2023-06-13 22:40:19 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navbar';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <div style={{display:'flex', justifyItems:'flex-star'}}>
+      <Button variant="contained" color='success'>Nosotros</Button>
+      <Button variant="contained" color='success'>Productos</Button>
+      <Button variant="contained" color='success'>Ubicacion</Button>
+      </div>
     </div>
   );
 }
